ba75a607a987536ea53d42f1edaedc704db989ae
John Escalona <johnrichmondescalona@gmail.com>
2019-10-14 21:22:15 -0700

    Update FCFS.java

--- CS4310-Operating-Systems/Project01/src/FCFS.java
+++ CS4310-Operating-Systems/Project01/src/FCFS.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
+import java.util.*;
 
 class FCFS {
     public static void main(String[] args) {
@@ -49,41 +50,41 @@ class FCFS {
         }
 
         // run job and output to file //
-        //runToFile(args);
+        runToFile(args[0]);
     }
 
-    public static void runToFile(String[] size) {
+    public static void runToFile(String size) {
         String jobName = "";
         int timeToFin = 0;
         int startTime = 0;
         int endTime = 0;
         String status = "Completed @ ";
         try {
-            File output = new File("result_" + size[0] + "-" + size[1] + ".txt");
-            File input = new File("jobs/jobs_" + size[0] + "-" + size[1] + ".txt");
+            File output = new File("result_" + size + ".txt");
+            File input = new File("../jobs/job_" + size + ".txt");
             PrintWriter wr = new PrintWriter(output);
             BufferedReader reader = new BufferedReader(new FileReader(input));
 
             // Print Header for Schedule Table
-            wr.write("-----------------------------------------------------------------\n");
-            wr.write("|   Name   |   Start Time   |   End Time    |       Status      |\n");
-            wr.write("-----------------------------------------------------------------\n");
+            wr.write("      -----------------------------------------------------------------\n");
+            wr.write("      |   Name   |   Start Time   |   End Time    |       Status      |\n");
+            wr.write("      -----------------------------------------------------------------\n");
 
             while ((jobName = reader.readLine()) != null) {
                 timeToFin = Integer.parseInt(reader.readLine());
                 startTime = endTime;
                 endTime = startTime + timeToFin;
                 status += endTime;
-                wr.write(String.format("|   %-7s|        %-8d|      %-9d| %-18s|\n", jobName, startTime, endTime,
+                wr.write(String.format("       |   %-7s|        %-8d|      %-9d| %-18s|\n", jobName, startTime, endTime,
                         status));
                 status = "Completed @ ";
             }
-            wr.write("-----------------------------------------------------------------\n");
-            reader.close();
+            wr.write("      -----------------------------------------------------------------\n");
             wr.flush();
+            reader.close();
             wr.close();
         } catch (Exception e) {
-            System.err.println("File error " + e.getClass());
+            System.err.println("File error: " + e.getClass());
         }
     }
 }
