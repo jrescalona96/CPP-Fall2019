ab8a4bbf6eed34dea7dbc25f9e6a70487aa9e7a2
John Escalona <johnrichmondescalona@gmail.com>
2019-10-27 22:14:48 -0700

    Revised pi_threads.cpp for Parallel Prog. to include all data sets

--- CS3700-Parallel-Programming/Projects/Proj02/cs3700_proj02/pi_threads.cpp
+++ CS3700-Parallel-Programming/Projects/Proj02/cs3700_proj02/pi_threads.cpp
@@ -1,3 +1,5 @@
+// Number of threads should be supplied upon execution as arguments
+
 #include <cmath>
 #include <chrono>
 #include <iostream>
@@ -9,31 +11,14 @@
 
 using namespace std;
 
+// DECLARATIONS //
+// Number of Trials
 const double size = 1000000000.0;
+// mutex lock
 mutex lock_mutex;
 
-// FUNTIONS //
-void runSimulation(double &totalHits, int id, unsigned long long start, unsigned long long end)
-{
-    double localHits;
-    double x, y, c;
-    long long localSlice = end - start;
-    srand((unsigned)id);
-    for (int i = 0; i <= localSlice; i++)
-    {
-        x = ((double)rand() / (double)RAND_MAX); // generate random x coordinate
-        y = ((double)rand() / (double)RAND_MAX); // generate random y coordinate
-        c = sqrt((x * x) + (y * y));
-        if (c <= 1.0)
-        {
-            localHits++;
-        }
-    }
-    // set lock automatically will be unlocked after
-
-    lock_guard<mutex> local_lock(lock_mutex);
-    totalHits += localHits;
-}
+// HEADER //
+void runSimulation(double &totalHits, int id, unsigned long long start, unsigned long long end);
 
 // MAIN //
 int main(int argc, char **argv)
@@ -47,6 +32,7 @@ int main(int argc, char **argv)
         int threadID;
 
         auto start = chrono::system_clock::now();
+
         unsigned long long slice = size / threads;
         unsigned long long startIndex = 0, endIndex = slice - 1;
         printf("\nSlice = %lld\n\n", slice);
@@ -55,33 +41,66 @@ int main(int argc, char **argv)
             threadID = i;
             printf("Thread %d,  Slice: [%lld:%lld]\n", threadID, startIndex, endIndex);
             td[threadID] = thread(runSimulation, ref(totalHits), threadID, startIndex, endIndex);
-            startIndex = endIndex + 1;
             //TODO: Need to account for ONLY up to the given size.
-            if (i == (threads - 1))
+
+            startIndex = endIndex + 1;
+            // the next thread to be sent will be the last thread.
+            // current thread is one before the last one
+            if (threadID == (threads - 2))
             {
-                unsigned long long endIndex = size - 1;
+                endIndex = size - 1;
             }
             else
             {
+                // not last thread
                 endIndex = startIndex + slice - 1;
             }
         }
         printf("Created %d threads\n\n", threads);
 
+        // JOIN THREADS //
         for (int i = 0; i < threads; i++)
         {
             td[i].join();
             printf("Thread %d joined\n", i);
         }
 
-        // print results
+        // PRINT RESULTS //
         chrono::duration<double> elapseTime = chrono::system_clock::now() - start;
+
         cout << "\nExecution time: " << elapseTime.count() << " seconds" << endl;
         cout << "Result: " << totalHits << " out of " << size << endl;
         cout << "pi =  " << ((totalHits / size) * 4) << endl;
     }
     else
     {
+        // LOG ERROR //
         cout << "Invalid Input: must input valid number of Threads!" << endl;
     }
+}
+
+// RUN THE SIMULATIONS //
+void runSimulation(double &totalHits, int id, unsigned long long start, unsigned long long end)
+{
+    // DECLARATIONS //
+    double localHits;
+    double x, y, c;
+    long long localSlice = end - start;
+
+    // GENERATE RANDOM NUMBERS //
+    srand((unsigned)id);
+    for (int i = 0; i <= localSlice; i++)
+    {
+        x = ((double)rand() / (double)RAND_MAX); // generate random x coordinate
+        y = ((double)rand() / (double)RAND_MAX); // generate random y coordinate
+        c = sqrt((x * x) + (y * y));
+        if (c <= 1.0)
+        {
+            localHits++;
+        }
+    }
+    // ENTERING CRITICAL SECTION //
+    // set lock automatically will be unlocked after
+    lock_guard<mutex> local_lock(lock_mutex);
+    totalHits += localHits;
 }
