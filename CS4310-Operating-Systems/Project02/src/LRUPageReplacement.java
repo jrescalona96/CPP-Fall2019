0e3ac6b98890d69144aa9124219ecb2acf715b57
John Escalona <johnrichmondescalona@gmail.com>
2019-11-14 12:53:25 -0800

    Finished OS Proj2 Code

--- CS4310-Operating-Systems/Project02/src/LRUPageReplacement.java
+++ CS4310-Operating-Systems/Project02/src/LRUPageReplacement.java
@@ -9,41 +9,42 @@ import java.util.Stack;
 
 class LRUPageReplacement {
     public static void main(String[] args) {
-        
+
         try {
             // DECLARATIONS //
             int pageFrameSize = Integer.parseInt(args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(new File("reference_strings_" + pageFrameSize +".txt")));
+            BufferedReader reader = new BufferedReader(
+                    new FileReader(new File("reference_strings_" + pageFrameSize + ".txt")));
             String referenceString = "";
             String page = new String();
             Queue<String> pageTable;
-            int numberOfPageFault = 0; // counter
+            int numberOfPageFaults = 0; // counter
             // TRAVERSE FILE //
-            while(reader.readLine() != null) {
+            while (reader.readLine() != null) {
                 // INITIALIZATIONS //
                 pageTable = new LinkedList<String>();
                 pageFrameSize = Integer.parseInt(reader.readLine()); // Page Frame
                 reader.readLine(); // Skip header text
                 referenceString = reader.readLine(); // Read reference String
-                System.out.printf("\nPage Frame size: %d\n",  pageFrameSize);
+                System.out.printf("\nPage Frame size: %d\n", pageFrameSize);
                 // START JOB //
                 for (int i = 0; i < referenceString.length(); i++) {
                     // read next page
                     System.out.printf("\nRemaining String: %s", referenceString.substring(i, referenceString.length()));
                     page = String.valueOf(referenceString.charAt(i));
-                    //initialize pageTable
-                    if(pageTable.size() < pageFrameSize) {
+                    // initialize pageTable
+                    if (pageTable.size() < pageFrameSize) {
                         pageTable.add(page);
                     } else {
                         // check if page is Fault
-                        if(pageFault(page, pageTable)) {
-                            numberOfPageFault++; //incerement page fault counter
+                        if (pageFault(page, pageTable)) {
+                            numberOfPageFaults++; // incerement page fault counter
                             // upadate page table
                             System.out.printf("\nFault: %s\n", page);
-                            pageTable.remove(); //remove oldest page
-                            pageTable.add(page); //add new page
+                            pageTable.remove(); // remove oldest page
+                            pageTable.add(page); // add new page
                         } else {
-                            // everytime there is a page hit, move that element to end of queue  
+                            // everytime there is a page hit, move that element to end of queue
                             pageTable = updatePageTable(page, pageTable);
                         }
                     }
@@ -51,6 +52,7 @@ class LRUPageReplacement {
                     printPageTable(pageTable);
                 }
             }
+            System.out.println("Number of Page Faults = " + numberOfPageFaults);
             reader.close();
             System.out.println("\nAll jobs Done!");
 
@@ -62,39 +64,33 @@ class LRUPageReplacement {
     // HELPER FUNCTIONS //
     public static boolean pageFault(String page, Queue<String> pageTable) {
         boolean res = false;
-        if(!pageTable.contains(page)) {
+        if (!pageTable.contains(page)) {
             res = true;
         }
         return res;
     }
-    
+
     public static Queue<String> updatePageTable(String page, Queue<String> pageTable) {
-        Queue<String> tableQueue = new LinkedList<String>(pageTable);
-        //Stack<String> tempStack = new Stack<String>();
-        while(!tableQueue.isEmpty()) {
-            // check if current table page == to page
-            if(page != tableQueue.peek()) {
-                // remove and store to stack
-                tableQueue.add(tableQueue.remove());
+        Queue<String> pageTableCopy = new LinkedList<String>(pageTable);
+        for (int i = 0; i < pageTableCopy.size(); i++) {
+            // check if page from table == page
+            if (page.equals(pageTableCopy.peek())) {
+                // remove and push to back of queue
+                pageTableCopy.add(pageTableCopy.remove());
             } else {
                 // remove matching item form queue
-                tableQueue.remove();
+                pageTableCopy.remove();
             }
         }
-        // // add all elements of tempStack to tableQueue
-        // for (int i = 0; i < tempStack.size(); i++) {
-        //     tableQueue.add(tempStack.pop());
-        // }
-        //add page to end of queue
-        tableQueue.add(page);
-        // return tableQueue;
-        return tableQueue;
+        pageTableCopy.add(page);
+        // return pageTableCopy;
+        return pageTableCopy;
     }
 
     public static void printPageTable(Queue<String> pageTable) {
         Queue<String> temp = new LinkedList<String>(pageTable);
         System.out.println();
-        while(temp.size() > 0) {
+        while (temp.size() > 0) {
             System.out.print(temp.remove() + " | ");
         }
         System.out.println();
--- CS4310-Operating-Systems/Project02/src/OptimalPageReplacement.java
+++ CS4310-Operating-Systems/Project02/src/OptimalPageReplacement.java
@@ -7,52 +7,52 @@ import java.util.Stack;
 
 class OptimalPageReplacement {
     public static void main(String[] args) {
-        
+
         try {
             // DECLARATIONS //
             int pageFrameSize = Integer.parseInt(args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(new File("reference_strings_" + pageFrameSize +".txt")));
+            BufferedReader reader = new BufferedReader(
+                    new FileReader(new File("reference_strings_" + pageFrameSize + ".txt")));
             String referenceString = "";
             String page = new String();
             Queue<String> pageTable;
             int numberOfPageFault = 0;
 
             // TRAVERSE FILE //
-            while(reader.readLine() != null) {
+            while (reader.readLine() != null) {
                 // INITIALIZATIONS //
                 pageTable = new LinkedList<String>();
                 pageFrameSize = Integer.parseInt(reader.readLine()); // Page Frame
                 reader.readLine(); // Skip header text
                 referenceString = reader.readLine(); // Read reference String
 
-                System.out.printf("\nPage Frame size: %d\n",  pageFrameSize);
+                System.out.printf("\nPage Frame size: %d\n", pageFrameSize);
                 // START JOB //
                 for (int i = 0; i < referenceString.length(); i++) {
                     System.out.printf("\nRemaining String: %s", referenceString.substring(i, referenceString.length()));
                     // read next page
                     page = String.valueOf(referenceString.charAt(i));
-                    
-                    if(pageTable.size() < pageFrameSize) {
-                        //initialize pageTable
-                        pageTable.add(page); 
+
+                    if (pageTable.size() < pageFrameSize) {
+                        // initialize pageTable
+                        pageTable.add(page);
                     } else {
                         // check if page is Fault
-                        if(pageFault(page, pageTable)) {
+                        if (pageFault(page, pageTable)) {
                             // upadate page table
-                            numberOfPageFault++; //incerement page fault counter
+                            numberOfPageFault++; // incerement page fault counter
+                            pageTable = updatePageTable(referenceString.substring(i, referenceString.length()),
+                                    pageTable);
                             System.out.printf("\nFault: %s\n", page);
-                            pageTable.remove(); //remove oldest page
-                            pageTable.add(page); //add new page
-                        } else {
-                            page = findOptimalPageToReplace(page, pageTable);
-                            pageTable = updatePageTable(page, pageTable);
+                            pageTable.remove(); // remove latest page to appear
+                            pageTable.add(page); // add new page
                         }
                     }
                     // print current page table //
                     printPageTable(pageTable);
                 }
             }
-            
+
             reader.close();
             System.out.println("\nAll jobs Done!");
 
@@ -62,52 +62,42 @@ class OptimalPageReplacement {
     }
 
     // HELPER FUNCTIONS //
-
-    public static String findOptimalPageToReplace(String page, Queue<String> pageTable) {
-        Queue<String> tableQueue = new LinkedList<String>(pageTable);
-        String optimalPage = "";
-        System.out.println("Implement finding the most optimal page method!!!!");
-        return optimalPage;
-    }
     // Returns true if there is a page fault, false otherwise
     public static boolean pageFault(String page, Queue<String> pageTable) {
         boolean res = false;
-        if(!pageTable.contains(page)) {
+        if (!pageTable.contains(page)) {
             res = true;
         }
         return res;
     }
-    
+
     // Method returns a new Queue with the latest accessed page at the end
-    public static Queue<String> updatePageTable(String page, Queue<String> pageTable) {
-        Queue<String> tableQueue = new LinkedList<String>(pageTable);
-        Stack<String> tempStack = new Stack<String>();
-        while(!tableQueue.isEmpty()) {
+    public static Queue<String> updatePageTable(String referenceString, Queue<String> pageTable) {
+        Queue<String> pageTableCopy = new LinkedList<String>(pageTable); // copy input page table (good practice only)
+        String p;
+        for (int i = 0; i < referenceString.length(); i++) {
+            p = referenceString.substring(i, i); // read page
             // check if current table page == to page
-            if(page != tableQueue.peek()) {
-                // remove and store to stack
-                tempStack.push(tableQueue.remove());
-            } else {
-                // remove matching item form queue
-                tableQueue.remove();
+            if (pageTableCopy.contains(p)) {
+                // put at end of queue
+                for (String s : pageTableCopy) {
+                    if (!s.equals(p)) {
+                        pageTableCopy.add(pageTableCopy.remove());
+                    } else {
+                        pageTableCopy.remove();
+                    }
+                }
+                pageTableCopy.add(p);
             }
         }
-
-        // add all elements of tempStack to tableQueue
-        for (int i = 0; i < tempStack.size(); i++) {
-            tableQueue.add(tempStack.pop());
-        }
-        //add page to end of queue
-        tableQueue.add(page);
-
-        return tableQueue;
+        return pageTableCopy;
     }
 
-    //generic method to print Queue
+    // generic method to print Queue
     public static <T> void printPageTable(Queue<T> queue) {
         Queue<T> temp = new LinkedList<T>(queue);
         System.out.println();
-        while(temp.size() > 0) {
+        while (temp.size() > 0) {
             System.out.print(temp.remove() + " | ");
         }
         System.out.println();
