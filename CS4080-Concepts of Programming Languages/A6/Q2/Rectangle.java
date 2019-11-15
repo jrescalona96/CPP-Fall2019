553810f760f34fe5bb74f9980b0fe4db181431e6
John Escalona <johnrichmondescalona@gmail.com>
2019-11-14 22:03:37 -0800

    finished A6 Concepts

--- CS4080-Concepts of Programming Languages/A6/Q2/Rectangle.java
+++ CS4080-Concepts of Programming Languages/A6/Q2/Rectangle.java
@@ -1,38 +1,47 @@
 class Rectangle {
-    // fields
-    protected double l;
-    protected double w;
-    protected double area;
-
-    // constructors
-    public Rectangle() {
-        this.l = 0;
-        this.w = 0;
-    }
-
-    public Rectangle(double l, double w) {
-        this.l = l;
-        this.w = w;
-    }
-
-    // set methods
-    public void setL(double l) {
-        this.l = l;
-    }
-
-    public void setW(double w) {
-        this.w = w;
-    }
 
     // statically bound area()
-    public void staticArea() {
-         this.area = this.l * this.w;
-         System.out.printf("Rectangle staticArea(): %.2f sq. units\n" , this.area);
+    public static void staticArea(double l, double w) {
+        System.out.printf("Rectangle staticArea(): %.2f sq. units\n", l * w);
     }
 
     // dynamically bound area()
-    public void dynamicArea() {
-        this.area = this.l * this.w;
-        System.out.printf("Rectangle dynamicArea(): %.2f sq. units\n" , this.area);
+    public void dynamicArea(double l, double w) {
+        System.out.printf("Rectangle dynamicArea(): %.2f sq. units\n", l * w);
+    }
+}
+
+class Square extends Rectangle {
+    // dynamically bound area()
+    @Override
+    public void dynamicArea(double l, double w) {
+        System.out.printf("Square dynamicArea(): %.2f sq. units\n", l * w);
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        // declarations
+        final int size = 100; // test case repetetions
+        long startTime;
+        Rectangle square = new Square();
+
+        // call to dynamically bound method
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            square.dynamicArea(5, 5);
+        }
+        long dynamicTime = System.currentTimeMillis() - startTime;
+
+        // call to statically bound method
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            Rectangle.staticArea(5, 5);
+        }
+        long staticTime = System.currentTimeMillis() - startTime;
+
+        System.out.printf("\nTime to finish %d calls:\n", size);
+        System.out.printf("Static Method = %d ms\n", staticTime);
+        System.out.printf("Dynamic Method = %d ms\n", dynamicTime);
     }
 }
--- CS4080-Concepts of Programming Languages/A6/Q2/Square.java
+++ CS4080-Concepts of Programming Languages/A6/Q2/Square.java
@@ -1,23 +1,7 @@
 class Square extends Rectangle {
-
-    // constructor
-    public Square() {
-        super(0, 0);
-    }   
-
-    public Square(double e) {
-        super(e, e);
-    }   
-
-    // set methods
-    public void setEdge(double e) {
-        super.l = e;
-        super.w = e;
-    }
-    
+    // dynamically bound area()
     @Override
-    public void dynamicArea() {
-        this.area = this.l * this.w;
-        System.out.printf("Square dynamicArea(): %.2f sq. units\n" , this.area);
+    public void dynamicArea(double l, double w) {
+        System.out.printf("Square dynamicArea(): %.2f sq. units\n", l * w);
     }
 }
